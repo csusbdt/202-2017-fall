d00d275f7934b430c0fcfcae02489755eb1c55fe
David Turner <csusbdt@gmail.com>
2017-10-23 12:22:55 -0700

    X

--- which/main.cpp
+++ which/main.cpp
@@ -1,24 +1,64 @@
+#include <cassert>
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <cerrno>
 #include <cstring>
+#include <vector>
 
 using namespace std;
 
-int main(int argc, char * argv[]) {
-        const char * cmd = "echo $PATH";
+// Return a list of directories in the system path.
+vector<string> getDirList() {
+	string envPath = getenv("PATH");
 
-        FILE * pipe = popen(cmd, "r");
-        if (pipe == nullptr) {
-                cout << "error: " << strerror(errno) << endl;
-                return 1;
-        }
+	// Replace colons with new lines.
+	for (int i = 0; i < envPath.size(); ++i) {
+		if (envPath[i] == ':') envPath[i] = '\n';
+	}
 
-	//char path[200];
-        char c;
-        while (fread(&c, 1, 1, pipe) == 1) {
-		cout << c << endl;
-        }
-        fclose(pipe);
+	stringstream ss(envPath);
+	string path;
+	vector<string> dirList;
+	while (ss.good()) {
+		ss >> path;
+		dirList.push_back(path);
+	}
+	return dirList;
 }
+
+// Return a list of files in the given directory.
+vector<string> getFileList(string dirPath) {
+	string cmd = "ls " + dirPath;
+	FILE * pipe = popen(cmd.c_str(), "r");
+	if (pipe == nullptr) {
+		cout << "error: " << strerror(errno) << endl;
+		exit(1);
+	}
+	vector<string> fileList;
+	char filename[1024];
+	while (fgets(filename, 1024, pipe) != nullptr) {
+		int len = strlen(filename);
+		filename[len - 1] = '\0';
+		fileList.push_back(string(filename));
+	}
+        fclose(pipe);
+	return fileList;
+}
+
+int main(int argc, char * argv[]) {
+	if (argc < 2) return 0;
+	string cmd = argv[1];
+	vector<string> dirList = getDirList();
+	for (int i = 0; i < dirList.size(); ++i) {
+		cout << dirList[i] << endl;
+		vector<string> fileList = getFileList(dirList[i]);
+		for (int j = 0; j < fileList.size(); ++j) {
+			if (fileList[j] == "ls") {
+				cout << "found" << endl;
+			}
+		}
+	}
+}
+
 
